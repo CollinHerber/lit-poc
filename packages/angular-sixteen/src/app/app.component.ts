e00a23933cf7a324935a0be3a47e32195b25f5b6
Collin Herber <rustaddicts@gmail.com>
2024-04-04 15:03:36 -0400

    feat: add angular 16 example

---
+++ packages/angular-sixteen/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import '../../../lit-vite/dist/assets/index-BQk9VHok';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-sixteen';
+}
