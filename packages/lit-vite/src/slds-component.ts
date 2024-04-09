3a4ccc2f3b22446e3a5139633a68f0a23c109dd8
Collin Herber <rustaddicts@gmail.com>
2024-04-09 11:48:05 -0400

    chore: remove unused leftover files

--- packages/lit-vite/src/slds-component.ts
+++
@@ -1,6 +0,0 @@
-import { unsafeCSS } from 'lit'
-import styles from '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css?inline';
-
-export class SldsComponent {
-  static styles = [unsafeCSS(styles)];
-}
